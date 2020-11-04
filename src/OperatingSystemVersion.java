806acb9c41fffeb980be1780e09b60488da457c9
Romain Agostinelli <romain.agostinelli>
2020-11-04 15:19:32 +0100

    Clean unused getters and make properties final

--- src/OperatingSystemVersion.java
+++ src/OperatingSystemVersion.java
@@ -7,26 +7,14 @@
  * @since 02.11.2020
  */
 public class OperatingSystemVersion {
-    private int major;
-    private int minor;
-    private int patch;
+    private final int major;
+    private final int minor;
+    private final int patch;
 
     public OperatingSystemVersion(int major, int minor, int patch) {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
-    }
-
-    public int getMajor() {
-        return major;
-    }
-
-    public int getMinor() {
-        return minor;
-    }
-
-    public int getPatch() {
-        return patch;
     }
 
     @Override
