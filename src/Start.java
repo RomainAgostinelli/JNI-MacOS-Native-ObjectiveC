dfdcec7f0f7a544e01c07d7eac457c19e2e164dd
Romain Agostinelli <romain.agostinelli>
2020-11-04 15:18:25 +0100

    Add comments

--- src/Start.java
+++ src/Start.java
@@ -1,11 +1,13 @@
 public class Start {
 
+    // load the library
     static {
         System.loadLibrary("osAdapter");
     }
 
     public static void main(String[] args) {
         OSApiAdapter adapter = new OSApiAdapter();
+        // simple use of a native function
         System.out.println(adapter.getOsVersion());
     }
 }
